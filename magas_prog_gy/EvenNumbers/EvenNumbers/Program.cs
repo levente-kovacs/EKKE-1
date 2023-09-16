3edac2b7a6e93d4f54705495aeec8c375a21c648
Levente Kovacs <nagyklevi@gmail.com>
2023-09-16 17:50:31 +0200

    MagasProg Gy 09. 16.

---
+++ magas_prog_gy/EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int count;
+
+do
+{
+    Console.WriteLine("Hány darab számot szeretnél beolvastatni?");
+} while (!int.TryParse(Console.ReadLine(), out count) || count <= 0);
+
+List<int> numbers = new List<int>();
+
+{
+    int currentNumber;
+    for (int i = 0; i < count; i++)
+    {
+        do
+        {
+            Console.WriteLine($"Add meg a(z) {i + 1}. számot!");
+        } while (!int.TryParse(Console.ReadLine(), out currentNumber) || !(currentNumber > -2000 && currentNumber < 2000) || !(currentNumber % 2 == 0));
+        numbers.Add(currentNumber);
+    }
+}
+
+Console.WriteLine($"\nA megadott számok összege: {numbers.Sum()}");
+Console.WriteLine($"A megadott számok átlaga: {numbers.Average()}");
+
+Console.WriteLine("Nyomj le egy billentyűt a kilépéshez....");
+Console.ReadKey();
---
+++ magas_prog_gy/Flats_09_16/Flats/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+string address1;
+string address2;
+
+do
+{
+    Console.WriteLine("Add meg az 1. lakás címét!");
+    address1 = Console.ReadLine();
+} while (address1.Length > 5);
+
+do
+{
+    Console.WriteLine("Add meg az 2. lakás címét!");
+    address2 = Console.ReadLine();
+} while (address2.Length > 5);
+
+
+float area1;
+float area2;
+do
+{
+    Console.WriteLine($"Add meg a(z) {address1} cím alatti lakás területét (m2-ben)!");
+
+} while (!float.TryParse(Console.ReadLine(), out area1) || area1 < 1);
+
+do
+{
+    Console.WriteLine($"Add meg a(z) {address2} cím alatti lakás területét (m2-ben)!");
+
+} while (!float.TryParse(Console.ReadLine(), out area2) || area2 < 1);
+
+
+int price;
+do
+{
+    Console.WriteLine($"Add meg a négyzetméter árat (Ft)!");
+
+} while (!int.TryParse(Console.ReadLine(), out price) || area2 <= 0);
+
+
+int flat1Price = (int)Math.Round(area1 * price);
+int flat2Price = (int)Math.Round(area2 * price);
+
+if (flat1Price > 40000000 && flat2Price > 40000000)
+{
+    Console.WriteLine("Tök mindegy, melyik drágább, úgysem tudom megvenni.");
+}
+else if (flat1Price == flat2Price)
+{
+    Console.WriteLine($"A(z) {address1} és a(z) {address2} alatti lakások ugyanannyiba kerülnek.");
+}
+else
+{
+    if (flat1Price > flat2Price)
+    {
+        Console.WriteLine($"A(z) {address1} alatti lakás drágább, mint a(z) {address2} alatti lakás.");
+    }
+    else
+    {
+        Console.WriteLine($"A(z) {address2} alatti lakás drágább, mint a(z) {address1} alatti lakás.");
+    }
+}
+
+
+
+
+Console.WriteLine("Nyomj le egy billentyűt...");
+Console.ReadKey();
