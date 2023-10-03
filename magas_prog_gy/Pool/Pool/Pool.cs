28128ab87a7020eb5f03efac8eba146c3ca0a94f
Levente Kovacs <nagyklevi@gmail.com>
2023-10-03 15:25:51 +0200

    Pools task done

---
+++ magas_prog_gy/Pool/Pool/Pool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pools
+{
+    internal class Pool
+    {
+        public double R { get; set; }
+        public double H { get; set; }
+
+        public double CalcVolume()
+        {
+            return R * R * Math.PI * H;
+        }
+
+        public Pool(double r, double h)
+        {
+            this.R = r;
+            this.H = h;
+        }
+
+        public override string? ToString()
+        {
+            return $"radius {R}m and height {H}m";
+        }
+    }
+}
---
+++ magas_prog_gy/Pool/Pool/Program.cs
@@ -0,0 +1,46 @@
+﻿using Pools;
+
+Pool GetPoolData()
+{
+    double poolR;
+    double poolH;
+    do
+    {
+        Console.Write("Enter the radius of the pool: ");
+    } while (!double.TryParse(Console.ReadLine(), out poolR) || poolR < 1);
+
+    do
+    {
+        Console.Write("Enter the height of the pool: ");
+    } while (!double.TryParse(Console.ReadLine(), out poolH) || poolH < 1);
+
+    Pool pool = new Pool(poolR, poolH);
+    return pool;
+}
+
+Console.WriteLine("First pool");
+Pool pool1 = GetPoolData();
+double pool1Volume = pool1.CalcVolume();
+Console.WriteLine($"The first pool can hold up to {pool1Volume} m3 water.");
+
+Console.WriteLine("\nSecond pool");
+Pool pool2 = GetPoolData();
+double pool2Volume = pool2.CalcVolume();
+Console.WriteLine($"The second pool can hold up to {pool2Volume} m3 of water.");
+
+Console.WriteLine();
+if (pool1Volume > pool2Volume)
+{
+    Console.WriteLine($"\nBuy the pool with {pool1}.");
+}
+else if (pool1Volume < pool2Volume)
+{
+    Console.WriteLine($"\nBuy the pool with {pool2}.");
+}
+else
+{
+    Console.WriteLine($"No matter which pool you buy, both can store {pool1Volume} m3 of water");
+}
+
+
+Console.ReadKey();
